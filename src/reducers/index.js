e16ec5744cccc8f2986e38743d53f6fdae3d0d51
Badri <badri.py@outlook.com>
2019-07-08 12:18:19 +0530

    Add the placeholder for user and book reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
+import books from './bookReducer';
+import user from './userReducer';    
 
 const rootReducer = combineReducers({
-
+    books,
+    user
 });
 
 export default rootReducer;
