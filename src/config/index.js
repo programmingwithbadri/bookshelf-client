2e5c9f0549951945efa67d895c9e6f4fd7d08b04
Badri <badri.py@outlook.com>
2019-08-27 22:34:22 +0530

    Revert the changes regarding secure URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 let serverUrl;
 if(process.env.NODE_ENV === 'production') {
-    serverUrl = 'http://pacific-springs-96663.herokuapp.com'
+    serverUrl = 'https://pacific-springs-96663.herokuapp.com'
   } else {
-    serverUrl =  'http://localhost:3001'
+    serverUrl =  'https://localhost:3001'
   }
 
   export default serverUrl;
